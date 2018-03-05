7d1007cd5bc0506fa0545cd6efdebba6ee8b3c85
daneil <263392076@qq.com>
2018-03-05 09:16:14 +0800

    a

--- ProceduralDigitalCash/C#demo/Huobi.Rest.CSharp.Demo/Model/HBResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Huobi.Rest.CSharp.Demo.Model
-{
-    public class HBResponse<T> where T : new()
-    {
-        public string Status { get; set; }
-        public T Data { get; set; }
-    }
-}
--- ProceduralDigitalCash/C#demo/WFDigitalCash/Huobi/HBResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WFDigitalCash
-{
-    public class HBResponse<T> where T : new()
-    {
-        public string Status { get; set; }
-        public T Data { get; set; }
-    }
-}
